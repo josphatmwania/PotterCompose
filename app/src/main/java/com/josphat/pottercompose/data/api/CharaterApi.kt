7c6287d5fae22510069d936acf66462bc74d9e48
josphatmwania <josphatmwania237@gmail.com>
2023-04-03 17:16:57 +0300

    Add getCharacter fun

--- app/src/main/java/com/josphat/pottercompose/data/api/CharaterApi.kt
+++ app/src/main/java/com/josphat/pottercompose/data/api/CharaterApi.kt
@@ -1,4 +1,9 @@
 package com.josphat.pottercompose.data.api
 
+import com.josphat.pottercompose.data.api.model.Character
+import retrofit2.http.GET
+
 interface CharaterApi {
+    @GET(ApiConstants.END_POINTS)
+    suspend fun getCharacter() : List<Character>
 }
