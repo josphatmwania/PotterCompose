583cfcf41e4e0ac2db8838566e62ee105acd0e99
josphatmwania <josphatmwania237@gmail.com>
2023-04-04 11:24:30 +0300

    Add CharacterImageCard to load images

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -1,16 +1,21 @@
 package com.josphat.pottercompose.ui.home
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
 import com.josphat.pottercompose.data.api.model.Character
 
 @Composable
@@ -36,5 +41,21 @@ fun HomeScreen() {
 
     }
 }
+
+/**
+ * Using COil to load images
+ */
+@Composable
+fun CharacterImageCard(character: Character) {
+    val imagePainter = rememberImagePainter(data = character.image)
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(16.dp)
+    ) {
+
+    }
+}
+
 
 
