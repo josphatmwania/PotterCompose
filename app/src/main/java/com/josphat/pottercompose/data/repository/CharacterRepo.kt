53aefd8066908ae598998b7ce41ee4f85388d5bf
josphatmwania <josphatmwania237@gmail.com>
2023-04-04 08:14:08 +0300

    Add getCharacters function

--- app/src/main/java/com/josphat/pottercompose/data/repository/CharacterRepo.kt
+++ app/src/main/java/com/josphat/pottercompose/data/repository/CharacterRepo.kt
@@ -1,4 +1,14 @@
 package com.josphat.pottercompose.data.repository
 
-class CharacterRepo {
+import com.josphat.pottercompose.data.api.CharaterApi
+import com.josphat.pottercompose.data.api.model.Character
+import javax.inject.Inject
+
+class CharacterRepo @Inject constructor(
+    private val charaterApi: CharaterApi
+){
+
+    suspend fun getCharacters():List<Character>{
+        return charaterApi.getCharacter()
+    }
 }
