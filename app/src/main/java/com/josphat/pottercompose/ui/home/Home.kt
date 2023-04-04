84b6cd8e0556b825210c546374ea801680324722
josphatmwania <josphatmwania237@gmail.com>
2023-04-04 11:13:49 +0300

    Add homeScreen Composable

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -1,2 +1,33 @@
 package com.josphat.pottercompose.ui.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun homeScreen() {
+    val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
+    val state by homeViewModel.state.collectAsState()
+
+    LazyColumn {
+        if (state.isEmpty()) {
+            item {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(align = Alignment.Center)
+                )
+
+            }
+        }
+
+    }
+}
