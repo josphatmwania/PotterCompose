6d45962f39a30062ba47eb6d3e3223d5952995b6
josphatmwania <josphatmwania237@gmail.com>
2023-04-06 20:50:21 +0300

    Modify the UI

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -4,15 +4,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
@@ -51,14 +56,17 @@ fun CharacterImageCard(character: Character) {
     val imagePainter = rememberAsyncImagePainter(model = character.image)
 
     Card(
-        shape = MaterialTheme.shapes.medium,
-        modifier = Modifier.padding(16.dp)
+//        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(16.dp),
+        shape = RoundedCornerShape(size = 10.dp)
+
+
     ) {
         Box{
 
             Image(painter = imagePainter, contentDescription = null,
             modifier = Modifier
-                .fillMaxSize()
+                .clip(shape = CircleShape)
                 .height(150.dp),
             contentScale = ContentScale.FillBounds
             )
@@ -75,9 +83,13 @@ fun CharacterImageCard(character: Character) {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(4.dp)
+
                 ) {
+
                     Text(text = "Real Name: ${character.actor} ")
                     Text(text = "Actor Name: ${character.name} ")
+
+
                     
                     
                 }
