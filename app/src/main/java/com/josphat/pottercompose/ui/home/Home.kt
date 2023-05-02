aa1e6077a879769f19431780d90e7857d46074fa
josphatmwania <josphatmwania237@gmail.com>
2023-05-02 11:35:42 +0300

    Fix Position

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -4,17 +4,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.josphat.pottercompose.data.api.model.Character
+import androidx.compose.foundation.Image
+import coil.compose.AsyncImagePainter.State.Empty.painter
+
 
 @Composable
 fun HomeScreen() {
@@ -42,32 +50,45 @@ fun HomeScreen() {
 }
 
 /**
- * Using COil to load images
+ * Using Coil to load images
  */
 
 
 @Composable
 fun CharacterImageCard(character: Character) {
-    val imagePainter = rememberAsyncImagePainter(model = character.image)
-    
+
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+
+//        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Start,
+
+
     )
 
     {
-        Image(painter = imagePainter, contentDescription = null)
+        Image(
+            modifier = Modifier
+                .width(190.dp)
+                .clip(RoundedCornerShape(50.dp))
+                .height(180.dp),
 
-        // Add modifier later
-        
+            painter = rememberImagePainter(data = character.image),
+            contentDescription = null )
+
         Column(
             modifier = Modifier
-            .padding(80.dp))
+            .padding(15.dp))
 
         {
             Text(text = "Real Name: ${character.actor}")
             Text(text = "Actor Name: ${character.name}")
         }
+
+
+
+
+
+
 
 
         
