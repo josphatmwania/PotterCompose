7d9991c1bcf901db50f183842e1f09d7bb437d33
josphatmwania <josphatmwania237@gmail.com>
2023-05-02 12:35:11 +0300

    Remove unused imports

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -6,23 +6,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import com.josphat.pottercompose.data.api.model.Character
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.shape.CircleShape
-import coil.compose.AsyncImagePainter.State.Empty.painter
 
 
 @Composable
@@ -71,7 +65,7 @@ fun CharacterImageCard(character: Character) {
                 .width(100.dp)
                 .clip(shape = CircleShape)
 //                .clip(CircleShape(50.dp))
-                .padding(25.dp)
+                .padding(10.dp)
                 .height(75.dp),
 
             painter = rememberImagePainter(data = character.image),
