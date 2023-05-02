840ccb4f548656c958bfc2f125ba69e5e093e403
josphatmwania <josphatmwania237@gmail.com>
2023-05-02 09:11:27 +0300

    Fix imports

--- app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/home/Home.kt
@@ -13,17 +13,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.josphat.pottercompose.data.api.model.Character
 
 @Composable
