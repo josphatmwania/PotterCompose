01a46d81859d75b4c39bacdd39280dd9784cfaab
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 15:25:38 +0300

    Add AppNavigation logic

--- app/src/main/java/com/josphat/pottercompose/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/navigation/AppNavigation.kt
@@ -1,11 +1,24 @@
 package com.josphat.pottercompose.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.input.key.Key.Companion.Home
+import androidx.navigation.NavHost
 
 
 @Composable
 fun AppNavigation() {
 
     val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoutes.Home.route) {
+        Home(navController)
+    }
+    composable(NavRoutes.Details.route + "/{id}") {
+        Details(
+            onBackPressed = {
+                navController.popBacktack()
+            }
+        )
+    }
 
 }
