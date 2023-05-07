5de24f963762d0a41d26d05b2f54a4f0dc104499
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 11:09:20 +0300

    INject Character repo

--- app/src/main/java/com/josphat/pottercompose/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/detail/DetailViewModel.kt
@@ -1,4 +1,10 @@
 package com.josphat.pottercompose.ui.detail
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import com.josphat.pottercompose.data.repository.CharacterRepo
+import javax.inject.Inject
+
+class DetailViewModel  @Inject constructor(
+    private val characterRepo: CharacterRepo
+) : ViewModel() {
 }
