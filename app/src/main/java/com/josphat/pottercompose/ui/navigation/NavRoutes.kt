f57c3eb3ddea58b71a752cdd115ec7f4b9e628a9
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 14:57:03 +0300

    Add navigation routes for Home and detail screens

--- app/src/main/java/com/josphat/pottercompose/ui/navigation/NavRoutes.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/navigation/NavRoutes.kt
@@ -2,6 +2,6 @@ package com.josphat.pottercompose.ui.navigation
 
 
 sealed class NavRoutes(val route: String) {
-    object Home : NavRoutes("HomeScreen")
-    object Details: NavRoutes("")
+    object Home : NavRoutes("Home_screen")
+    object Details: NavRoutes("Detail_screen")
 }
