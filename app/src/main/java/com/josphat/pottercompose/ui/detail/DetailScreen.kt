c143b6b101531f75093d82068c4a71e8c90e149d
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 18:18:56 +0300

    Add row alignment

--- app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
@@ -1,10 +1,13 @@
 package com.josphat.pottercompose.ui.detail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.josphat.pottercompose.R
@@ -32,6 +35,12 @@ fun Details(
 
         }
     ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+
+        }
         
     }
 
