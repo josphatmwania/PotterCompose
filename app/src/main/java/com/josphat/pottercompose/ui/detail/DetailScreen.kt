6f8e8bd610506e5eaaa0260726d681d37336df96
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 18:09:08 +0300

    Add arrowback resource

--- app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
@@ -1,13 +1,39 @@
 package com.josphat.pottercompose.ui.detail
 
+import androidx.compose.foundation.Image
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.josphat.pottercompose.R
 
 @Composable
 fun Details(
     viewModel: DetailViewModel = hiltViewModel(),
     onBackPressed : () -> Unit
 ) {
+    val state = viewModel.state.value
+    
+    
+    Scaffold(  
+        topBar = {
+            TopAppBar( 
+                navigationIcon = {
+                    IconButton(onClick = { onBackPressed() }) {
+                        Image(painter = painterResource(id = R.drawable.arrow_back), contentDescription = "")
+                        
+                    }
+                },
+                title = {}
+            )
+                
+
+        }
+    ){
+        
+    }
 
 }
 
