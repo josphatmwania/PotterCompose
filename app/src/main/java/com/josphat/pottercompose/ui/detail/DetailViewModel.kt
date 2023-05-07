557be9475dc330632afbbb4321b396537151e8f4
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 14:49:44 +0300

    Add AppNavigation

--- app/src/main/java/com/josphat/pottercompose/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/detail/DetailViewModel.kt
@@ -2,9 +2,13 @@ package com.josphat.pottercompose.ui.detail
 
 import androidx.lifecycle.ViewModel
 import com.josphat.pottercompose.data.repository.CharacterRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailViewModel  @Inject constructor(
     private val characterRepo: CharacterRepo
 ) : ViewModel() {
+
+
 }
