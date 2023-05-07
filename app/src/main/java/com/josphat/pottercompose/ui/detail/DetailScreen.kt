c7b62dc46af988c7382a5981731ca142b7052849
josphatmwania <josphatmwania237@gmail.com>
2023-05-07 17:56:54 +0300

    Add Detail viewmodel intance

--- app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/detail/DetailScreen.kt
@@ -1,2 +1,13 @@
 package com.josphat.pottercompose.ui.detail
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun Details(
+    viewModel: DetailViewModel = hiltViewModel(),
+    onBackPressed : () -> Unit
+) {
+
+}
+
--- app/src/main/java/com/josphat/pottercompose/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/josphat/pottercompose/ui/navigation/AppNavigation.kt
@@ -3,7 +3,7 @@ package com.josphat.pottercompose.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHost
 import androidx.navigation.compose.rememberNavController
-
+import androidx.navigation.compose.ComposeNavigator
 
 
 @Composable
