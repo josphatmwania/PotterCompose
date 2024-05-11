2e43cc4d3c5c72f204f0d6719faf8193343916de
josphatmwania <josphatmwania237@gmail.com>
2024-05-11 20:38:46 +0300

    feat: implement CharacterEntity class

--- app/src/main/java/com/josphat/pottercompose/db/entities/CharacterEntity.kt
+++ app/src/main/java/com/josphat/pottercompose/db/entities/CharacterEntity.kt
@@ -1,5 +1,29 @@
 package com.josphat.pottercompose.db.entities
 
+import androidx.room.Entity
 
 
-data class CharacterEntity()
+@Entity(tableName = "characters_table")
+data class CharacterEntity(
+    val actor: String?,
+    val alive: Boolean?,
+    val alternate_actors: List<String>?,
+    val alternate_names: List<String>?,
+    val ancestry: String?,
+    val dateOfBirth: String?,
+    val eyeColour: String?,
+    val gender: String?,
+    val hairColour: String?,
+    val hogwartsStaff: Boolean?,
+    val hogwartsStudent: Boolean?,
+    val house: String?,
+    @PrimaryKey(autoGenerate = false)
+    val id: String,
+    val image: String?,
+    val name: String?,
+    val patronus: String?,
+    val species: String?,
+    val wand: Wand?,
+    val wizard: Boolean?,
+    val yearOfBirth: Int?
+)
