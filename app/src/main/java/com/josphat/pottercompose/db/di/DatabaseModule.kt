57c4d401014a96f3f57888609ac5da8655c419c0
josphatmwania <josphatmwania237@gmail.com>
2024-05-11 20:18:55 +0300

    feat: implement DatabaseModule dependency injection

--- app/src/main/java/com/josphat/pottercompose/db/di/DatabaseModule.kt
+++ app/src/main/java/com/josphat/pottercompose/db/di/DatabaseModule.kt
@@ -1,4 +1,44 @@
 package com.josphat.pottercompose.db.di
 
+import androidx.room.Room
+import androidx.room.TypeConverters
+import com.google.gson.Gson
+import com.josphat.pottercompose.db.PotterDB
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+
+    fun provideGson() = Gson()
+
+    @Provides
+    @Singleton
+    fun provideTyppeConverters(gson: Gson) = Converters(gson)
+
+
+    @Peovides
+    @Singleton
+    fun providePotterDB(
+        @ApplicationContext context: ApplicationContext,
+        converters: Converters
+    ) : PotterDB {
+        return Room.databaseBuilder(context.applicationContext,
+            PotterDB::class.java,
+            "PotterDB")
+            .fallbackToDestructiveMigration()
+            .addTypeConverter(converters)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun ProvideContractDao(database: PotterDB) = database.characterDao
 }
