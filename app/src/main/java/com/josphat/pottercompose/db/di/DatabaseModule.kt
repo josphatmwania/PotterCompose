05ace7bc30aceddafee664bced36855753988ee7
josphatmwania <josphatmwania237@gmail.com>
2024-05-20 10:39:34 +0300

    feat: Configure gradle issues

--- app/src/main/java/com/josphat/pottercompose/db/di/DatabaseModule.kt
+++ app/src/main/java/com/josphat/pottercompose/db/di/DatabaseModule.kt
@@ -1,17 +1,25 @@
 package com.josphat.pottercompose.db.di
 
+import android.content.Context
 import androidx.room.Room
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.google.gson.Gson
 import com.josphat.pottercompose.db.PotterDB
+import com.josphat.pottercompose.db.converters.Converters
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Singleton
+import dagger.hilt.InstallIn;
+
+
+
+
 
 
 @Module
 @InstallIn(SingletonComponent::class)
-
 object DatabaseModule {
 
     @Provides
@@ -24,10 +32,10 @@ object DatabaseModule {
     fun provideTyppeConverters(gson: Gson) = Converters(gson)
 
 
-    @Peovides
+    @Provides
     @Singleton
     fun providePotterDB(
-        @ApplicationContext context: ApplicationContext,
+        @ApplicationContext context: Context,
         converters: Converters
     ) : PotterDB {
         return Room.databaseBuilder(context.applicationContext,
@@ -40,5 +48,5 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun ProvideContractDao(database: PotterDB) = database.characterDao
+    fun provideContractDao(database: PotterDB) = database.characterDao
 }
